c71ae1583d557091ccfd2df30516933224e98f84
DessaWins <dessawins@gmail.com>
2016-06-04 08:52:07 +0000

    resenha

--- mvc/view/resenha.php
+++ mvc/view/resenha.php
@@ -1,30 +1,15 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>resenha</title>
+        
+    </head>
     <body>     
-    <table class="basic-table">
-	<thead>
-		<tr>
-			<td>filme</td>
-			<td>opiniao</td>
-			<td>tipo</td>
-			<td>video</td>
-			<td>autor</td>
-			
-		</tr>
-	</thead>
-	<tbody>
-   
-    <?php 
-    foreach($dado as $resenha){
-	    echo "<tr>";
-	    echo "<td>" . $resenha->getNome() . "</td>";
-        echo "<td>" . $resenha->getOpiniao() . "</td>";
-        echo "<td>" . $resenha->getTipo() . "</td>";
-        echo "<td>" . $resenha->getVideo() . "</td>";
-	    echo "<td>" . $resenha->getAutor() . "</td>";
-                    	
-            }
+     <?php
+        foreach($dado as $resenha){
+            echo "<div>" . $resenha->getNome() . "</div>";
+         }
         ?>
-    	</tbody>
-</table>
+    </body>
+      
 </html>
